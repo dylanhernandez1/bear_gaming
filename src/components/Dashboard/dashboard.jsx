a4403b318d2223de373e00b60587585c7a0d9468
Manmeet Gill <manmeetg2124@gmail.com>
2026-02-22 23:04:35 -0800

    Slider Buttons work and CSS for the buttons is a mess

--- src/components/Dashboard/dashboard.jsx
+++ src/components/Dashboard/dashboard.jsx
@@ -6,6 +6,7 @@ import left_arrow from "./left-arrow.png";
 import right_arrow from "./right-arrow.png";
 import "./dashboard.css"
 
+const width_size = 250;
 const data = [
 	{
 		src: fortnite
@@ -18,23 +19,44 @@ const data = [
 	}
 ];
 
+const totalWidth = width_size * data.length;
+const visibleWidth = 2.5 * width_size;
+const max_position = totalWidth - visibleWidth;
+
 const overlay_data = ["Fortnite", "Minecraft", "Roblox"];
 
 const Dashboard = () => {
-
+	const [position, setPosition] = useState(0);
 	const dashboardRef = useRef(null);
-	function onHandleClick () {
-		console.log('Button clicked');
+
+	function onHandleClick (change_amount) {
+		console.log("clicked");
+		console.log("Change amount:", change_amount);
+		setPosition(prev => {
+			const new_position = prev + change_amount;
+			console.log("New Position:", new_position);
+			if (new_position < 0)
+			{
+				return 0;
+			}
+			else if (new_position > max_position)
+			{
+				return max_position;
+			}
+			
+			return new_position;
+		});
 	}
+	
 
 	return (
-	<div className={"row"}>
-		<div className="container">
-			<button onClick={onHandleClick}>
+	<div className={"dashboard"}>
+		<div className="carousel">
+			<button onClick={() => onHandleClick(-visibleWidth)}>
 				<img src={left_arrow} alt="Clickable left button" className="slider-buttons"/>
 			</button>
 			
-			<div className="dashboard" ref={dashboardRef}>
+			<div className="row" ref={dashboardRef} style={{ transform: `translateX(-${position}px)` }}>
 				{data.map((v, i) => (
 					<div key={i} className="Slider">
 						<img src={v.src} className="dashboard-image" />
@@ -45,8 +67,8 @@ const Dashboard = () => {
 				))}
 			</div>
 			
-			<button onClick={onHandleClick}>
-				<img src={right_arrow} alt="Clickable left button" className="slider-buttons"/>
+			<button onClick={() => onHandleClick(visibleWidth)}>
+				<img src={right_arrow} alt="Clickable right button" className="slider-buttons"/>
 			</button>
 		</div>
 	</div>
