71babb274b252c15ed307103cd22acb4b346ead2
Manmeet Gill <manmeetg2124@gmail.com>
2026-02-22 18:50:44 -0800

    First images pushed and overlay is done

---
+++ src/components/Dashboard/dashboard.jsx
@@ -0,0 +1,45 @@
+import { useState, useEffect, useRef, createRef} from "react";
+import minecraft from "./dashboard_images/minecraft2048x2048.png";
+import roblox from "./dashboard_images/Roblox.png";
+import fortnite from "./dashboard_images/Fortnite.jpg"
+
+import "./dashboard.css"
+
+const data = [
+	{
+		src: fortnite
+	},
+	{
+		src: minecraft
+	},
+	{
+		src: roblox
+	}
+];
+
+const overlay_data = ["Fortnite", "Minecraft", "Roblox"];
+
+const Dashboard = () => {
+
+	const dashboardRef = useRef(null);
+
+
+	return (
+	<div className={"row"}>
+		<div className="container">
+			<div className="dashboard" ref={dashboardRef}>
+				{data.map((v, i) => (
+					<div key={i} className="Slider">
+						<img src={v.src} className="dashboard-image" />
+						<div className="overlay">
+							<div className="overlay-text">{overlay_data[i]}</div>
+						</div>
+					</div>
+				))}
+			</div>
+		</div>
+	</div>
+	);
+};
+
+export default Dashboard;
